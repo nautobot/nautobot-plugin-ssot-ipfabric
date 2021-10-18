3890017cbc4eb9409f45ee64b51e367ac5d26a5a
Jeremy White <jeremy.white@networktocode.com>
2021-10-18 14:43:00 -0500

    first commit

---
+++ nautobot_ssot_ipfabric/__init__.py
@@ -0,0 +1,30 @@
+"""Plugin declaration for nautobot_ssot_ipfabric."""
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class NautobotSSoTIPFabricConfig(PluginConfig):
+    """Plugin configuration for the nautobot_ssot_ipfabric plugin."""
+
+    name = "nautobot_ssot_ipfabric"
+    verbose_name = "Nautobot SSoT IPFabric"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "Nautobot SSoT IPFabric."
+    base_url = "ssot-ipfabric"
+    required_settings = []
+    min_version = "1.1.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
+
+
+config = NautobotSSoTIPFabricConfig  # pylint:disable=invalid-name
---
+++ nautobot_ssot_ipfabric/urls.py
@@ -0,0 +1,4 @@
+"""Django urlpatterns declaration for nautobot_ssot_ipfabric plugin."""
+# from django.urls import path
+
+urlpatterns = []
