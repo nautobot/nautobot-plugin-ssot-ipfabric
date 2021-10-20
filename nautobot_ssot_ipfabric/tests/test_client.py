6a891dea1d8834ba2b64631e4c6dc5e448632e72
Hugo Tinoco <43675476+h4ndzdatm0ld@users.noreply.github.com>
2021-10-20 11:47:48 -0700

    Additional ipfab client tested (#27)
    Co-authored-by: Hugo <hugo.tinoco@networktocode.com>

---
+++ nautobot_ssot_ipfabric/tests/test_client.py
@@ -0,0 +1,57 @@
+"""Test additional IP-Fabric Client Calls."""
+import os
+import unittest
+
+import responses
+from nautobot_ssot_ipfabric.utilities.ipfabric_client import IpFabricClient
+
+from nautobot_ssot_ipfabric.tests.fixtures import real_path
+from nautobot_ssot_ipfabric.utilities import json_fixture
+
+FIXTURES = os.environ.get("FIXTURE_DIR", real_path)
+
+
+class TestIpFabricClient(unittest.TestCase):
+    """Test IP Fabric Client"""
+
+    def setUp(self):
+        """Setup."""
+        self.uri = "https://ip-fabric-host.com"
+        self.client = IpFabricClient(self.uri, "CrazyTrainToken")
+
+    @responses.activate
+    def test_get_sites(self):
+        """Test `get_sites` API Call."""
+        endpoint = f"{self.uri}/api/v1/tables/inventory/sites"
+        json_response = json_fixture(f"{FIXTURES}/get_sites.json")
+        # IP Fabric Responses are wrapped inside 'data' key.
+        response = {"data": json_response}
+        responses.add(
+            responses.POST,
+            endpoint,
+            json=response,
+            status=200,
+        )
+        sites = self.client.get_sites()
+
+        self.assertEqual(sites[0]["siteName"], "JCY-RTR-01_1")
+        self.assertEqual(len(sites), 6)
+
+    @responses.activate
+    def test_get_device_inventory(self):
+        """Test `get_device_inventory` API Call."""
+        endpoint = f"{self.uri}/api/v1/tables/inventory/devices"
+        json_response = json_fixture(f"{FIXTURES}/get_device_inventory.json")
+        # IP Fabric Responses are wrapped inside 'data' key.
+        response = {"data": json_response}
+        responses.add(
+            responses.POST,
+            endpoint,
+            json=response,
+            status=200,
+        )
+        sites = self.client.get_device_inventory()
+
+        self.assertEqual(sites[0]["hostname"], "nyc-spine-02")
+        self.assertEqual(sites[0]["platform"], "veos")
+        self.assertEqual(len(sites), 6)
--- nautobot_ssot_ipfabric/utilities/__init__.py
+++ nautobot_ssot_ipfabric/utilities/__init__.py
@@ -1,15 +1,16 @@
 """Utilities."""
 from .nbutils import (
-    create_site,
-    create_region,
-    create_tenant,
-    create_device_type_object,
-    create_manufacturer,
     create_device_role_object,
     create_device_status,
-    create_ip,
+    create_device_type_object,
     create_interface,
+    create_ip,
+    create_manufacturer,
+    create_region,
+    create_site,
+    create_tenant,
 )
+from .test_utils import json_fixture
 
 __all__ = (
     "create_site",
@@ -21,4 +22,5 @@ __all__ = (
     "create_device_status",
     "create_ip",
     "create_interface",
+    "json_fixture",
 )
