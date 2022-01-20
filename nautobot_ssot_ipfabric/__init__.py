48065f996fdece31118dd6a971c4e262e893a996
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-01-20 09:22:27 -0500

    Black

--- nautobot_ssot_ipfabric/__init__.py
+++ nautobot_ssot_ipfabric/__init__.py
@@ -23,9 +23,7 @@ class NautobotSSoTIPFabricConfig(PluginConfig):
     required_settings = ["ipfabric_host", "ipfabric_api_token"]
     min_version = "1.1.0"
     max_version = "1.9999"
-    default_settings = {
-        "ipfabric_verify": True
-    }
+    default_settings = {"ipfabric_verify": True}
     caching_config = {}
 
 
